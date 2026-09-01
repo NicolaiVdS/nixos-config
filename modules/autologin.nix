e4ff9d0eb79bae2f2417a9debb7abe5445dcee1e
Nicolai Van der Storm <nvanderstorm@belki.be>
2026-09-01 13:51:25 +0200

    added dotfiles

--- modules/autologin.nix
+++ modules/autologin.nix
@@ -6,7 +6,13 @@
     # Launch Hyprland automatically when logging into tty1
     environment.loginShellInit = ''
       if [ -z "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
-        exec start-hyprland
+        echo -n "Starting Hyperland in 1s... (press ANY key to cancel)"
+        if read -t 1 -k 1 -s key; then
+          echo -e "\nBypassed Hyprland, Dropping into tty."
+        else
+          echo -e "\nLaunching Hyprland..."
+          exec start-hyprland
+        fi
       fi
     '';
   };
---
+++ modules/dotfiles.nix
@@ -0,0 +1,29 @@
+{ ... }:
+{
+  flake.modules.homeManager.dotfiles =
+    { config, pkgs, ... }:
+    let
+      dotfilesDir = "${config.home.homeDirectory}/dotfiles";
+      dotfilesRepo = "https://github.com/NicolaiVdS/dotfiles.git";
+    in
+    {
+      home.activation.syncDotfiles = config.lib.dag.entryBefore [ "checkLinkTargets" ] ''
+        if [ ! -d "${dotfilesDir}" ]; then
+          $DRY_RUN_CMD ${pkgs.git}/bin/git clone "${dotfilesRepo}" "${dotfilesDir}"
+        else
+          $DRY_RUN_CMD ${pkgs.git}/bin/git -C "${dotfilesDir}" pull --rebase
+        fi
+      '';
+
+      # Live symlinks
+      xdg.configFile = {
+        "ghostty".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/ghostty";
+        "hypr".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/hypr";
+        "nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/nvim";
+        "ohmyposh".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/ohmyposh";
+        "superfile".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/superfile";
+      };
+
+      home.file.".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.zshrc";
+    };
+}

59af42a15bce85569887382663ad50cbbd159f3e
Nicolai Van der Storm <nvanderstorm@belki.be>
2026-09-01 16:10:23 +0200

    link .zshrc during activation

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -9,13 +9,22 @@
     {
       home.activation.syncDotfiles = config.lib.dag.entryBefore [ "checkLinkTargets" ] ''
         if [ ! -d "${dotfilesDir}" ]; then
-          $DRY_RUN_CMD ${pkgs.git}/bin/git clone "${dotfilesRepo}" "${dotfilesDir}"
+          $DRY_RUN_CMD ${pkgs.git}/bin/git clone \
+            "${dotfilesRepo}" \
+            "${dotfilesDir}"
         else
-          $DRY_RUN_CMD ${pkgs.git}/bin/git -C "${dotfilesDir}" pull --rebase
+          $DRY_RUN_CMD ${pkgs.git}/bin/git -C \
+            "${dotfilesDir}" pull --rebase
+        fi
+
+        if [ ! -e "${config.home.homeDirectory}/.zshrc" ] ||
+           [ -L "${config.home.homeDirectory}/.zshrc" ]; then
+          $DRY_RUN_CMD ln -sfn \
+            "${dotfilesDir}/.zshrc" \
+            "${config.home.homeDirectory}/.zshrc"
         fi
       '';
 
-      # Live symlinks
       xdg.configFile = {
         "ghostty".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/ghostty";
         "hypr".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/hypr";
@@ -23,7 +32,5 @@
         "ohmyposh".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/ohmyposh";
         "superfile".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/superfile";
       };
-
-      home.file.".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.zshrc";
     };
 }

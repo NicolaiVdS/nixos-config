0c57368813b3771ac942099fe2c9cf55d5a56b3b
Nicolai Van der Storm <nvanderstorm@belki.be>
2026-09-01 12:57:50 +0200

    fix to auto launch hyprland

--- modules/autologin.nix
+++ modules/autologin.nix
@@ -2,13 +2,16 @@
 {
   flake.modules.nixos.autologin = { ... }: {
     services.getty.autologinUser = "nicolaivds";
+
+    # Launch Hyprland automatically when logging into tty1
+    environment.loginShellInit = ''
+      if [ -z "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
+        exec start-hyprland
+      fi
+    '';
   };
 
   flake.modules.homeManager.autologin = { ... }: {
-    programs.zsh.profileExtra = ''
-      if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
-        exec Hyprland
-      fi
-    '';
+    programs.zsh.enable = true;
   };
 }
